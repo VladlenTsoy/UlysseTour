ae2efa4f913f134a14140ec797de2c3035d82fd2
Vladlen <vladlengg@gmail.com>
2019-03-28 04:06:15 +0500

    Update styles

---
+++ app/Setting/MetaTag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ulyssetour\Setting;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MetaTag extends Model
+{
+    //
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/bootstrap.js', 'public/js')
     .js('resources/js/app.js', 'public/js')
+    // Style
+    .styles([
+        'resources/css/style.css',
+        'resources/css/tour.css',
+        'resources/css/tours.css',
+    ], 'public/css/style.css')
    .sass('resources/sass/app.scss', 'public/css');
