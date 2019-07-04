a2589ed833d184955eab0b0f7f31cb86bc0163a3
Vladlen <vladlengg@gmail.com>
2019-07-04 12:17:28 +0500

    add metrik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -577,6 +577,34 @@
         }
     })();</script>
 <!-- {/literal} END JIVOSITE CODE -->
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(54308733, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+
+<noscript><div><img src="https://mc.yandex.ru/watch/54308733" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143251293-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-143251293-1');
+</script>
+
 <script src="{{asset('js/bootstrap.js')}}"></script>
 <script src="{{asset('js/app.js')}}"></script>
 <script async defer
